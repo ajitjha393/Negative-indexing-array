e9d52e2f455401acfa9f19a9216aac90c4173ced
ajitjha393 <BJha@hostanalytics.com>
2022-01-12 12:56:04 +0530

    Add negative indexing set feat

--- test.js
+++ test.js
@@ -2,5 +2,13 @@ Object.setPrototypeOf(Array.prototype, new Proxy({}, {
    get: (t,p,r) => { 
 		i = Number(p)
 	    return (!Number.isNaN(i) && i < 0) ? r[i + r.length]: Reflect.get(t,p,r) 
+   },
+   set: (t,p,v,r) => {
+		i = Number(p)
+		if(!Number.isNaN(i) && i < 0) { 
+			p = String(i + r.length)
+		}
+		return Reflect.set(t,p,v,r)
    }
+   
 }))
